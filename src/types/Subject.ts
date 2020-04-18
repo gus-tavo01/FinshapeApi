7882a4cf0fb14c884174552e0692d32616ec21c7
Gustavo <gloera@dfsupply.com>
2020-04-18 11:30:59 -0700

    move model entities

---
+++ src/types/Subject.ts
@@ -0,0 +1,17 @@
+import { Point } from './Point';
+import { Position } from './Position';
+
+export interface Subject {
+  hashKey: string;
+  coordinates: Point[];
+  controlPoints: Point[];
+  location: string;
+  fileName: string;
+  rotation: number;
+  zoom: number;
+  position: Position;
+  flip: number;
+  area: number;
+  tags: string;
+  specieId: string;
+}
