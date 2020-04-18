642fe2bdac1ea5d4512c73132b576a0ad6dc40ae
Gustavo <gloera@dfsupply.com>
2020-04-18 11:30:13 -0700

    add Subject mongo schema

--- src/models/Subject.ts
+++ src/models/Subject.ts
@@ -1,17 +1,21 @@
-import { Point } from './Point';
-import { Position } from './Position';
+import { Document, Schema, model } from 'mongoose';
+import { Subject } from '../types/Subject';
 
-export interface Subject {
-  id: string;
-  coordinates: Point[];
-  controlPoints: Point[];
-  location: string;
-  fileName: string;
-  rotation: number;
-  zoom: number;
-  position: Position;
-  flip: number;
-  area: number;
-  tags: string;
-  specieId: string;
-}
+interface SubjectModel extends Document, Subject {}
+
+const SubjectSchema = new Schema({
+  hashKey: { type: String, required: true },
+  // coordinates: { type: Point[]}
+  // controlPoints: Point[];
+  // position: Position;
+  location: { type: String, required: false },
+  fileName: { type: String, required: true },
+  rotation: { type: Number, required: false },
+  zoom: { type: Number, required: false },
+  flip: { type: Number, required: false },
+  area: { type: Number, required: false },
+  tags: { type: String, required: false },
+  // specieId: string;
+});
+
+export const SubjectModel = model<SubjectModel>('Subjects', SubjectSchema);
