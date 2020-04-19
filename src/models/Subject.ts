78c051a4b8172dff5ddabe1deddc2ad474754cd1
Gustavo <gloera@dfsupply.com>
2020-04-18 18:34:40 -0700

    add subject schema and model

--- src/models/Subject.ts
+++ src/models/Subject.ts
@@ -1,13 +1,19 @@
 import { Document, Schema, model } from 'mongoose';
 import { Subject } from '../types/Subject';
+import { Collections } from './enums/collections';
 
-interface SubjectModel extends Document, Subject {}
+// nested schemas
+import { PointSchema } from './point';
+import { PositionSchema } from './position';
+import { Models } from './enums/Models';
+
+interface ISubject extends Document, Subject {}
 
 const SubjectSchema = new Schema({
-  hashKey: { type: String, required: true },
-  // coordinates: { type: Point[]}
-  // controlPoints: Point[];
-  // position: Position;
+  hashKey: { type: String, required: true, unique: true },
+  coordinates: [PointSchema],
+  controlPoints: [PointSchema],
+  position: PositionSchema,
   location: { type: String, required: false },
   fileName: { type: String, required: true },
   rotation: { type: Number, required: false },
@@ -15,7 +21,15 @@ const SubjectSchema = new Schema({
   flip: { type: Number, required: false },
   area: { type: Number, required: false },
   tags: { type: String, required: false },
-  // specieId: string;
+  specieId: {
+    type: Schema.Types.ObjectId,
+    ref: Models.specie,
+    required: false,
+  },
 });
 
-export const SubjectModel = model<SubjectModel>('Subjects', SubjectSchema);
+export const SubjectModel = model<ISubject>(
+  Models.subject,
+  SubjectSchema,
+  Collections.subjects
+);
