7f77e4843e521dbba27ac9af69e20bc51df8a201
Gustavo <gloera@dfsupply.com>
2020-04-18 18:34:04 -0700

    add basic subject repo methods

--- src/service/SubjectsRepository.ts
+++ src/service/SubjectsRepository.ts
@@ -1,12 +1,31 @@
-import { Repository } from './Repository';
 import { SubjectModel } from '../models/subject';
-import { Subject } from '../types/Subject';
 import { Service, Scope, ProviderScope } from '@tsed/common';
+import { Model, Document } from 'mongoose';
+import { Subject } from '../types/Subject';
+import { Collections } from '../models/enums/collections';
 
 @Service()
 @Scope(ProviderScope.INSTANCE)
-export class SubjectsRepository extends Repository<Subject> {
-  constructor() {
-    super(SubjectModel);
+export class SubjectsRepository {
+  public async add(entity: Subject): Promise<string | null> {
+    try {
+      const subject = new SubjectModel(entity);
+      const result = await subject.save();
+      return result.hashKey;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  public async getSingle(id: string): Promise<Subject | null> {
+    try {
+      const subject = await SubjectModel.findOne({ hashKey: id });
+
+      if (subject === null) return null;
+
+      return subject.populate(Collections.species);
+    } catch (err) {
+      return null;
+    }
   }
 }
