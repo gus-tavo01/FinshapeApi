06613ac5a2d03b096082a5b60b7fe24848addabd
Gustavo <gloera@dfsupply.com>
2020-04-19 17:33:35 -0700

    extend interfaces on document entities

--- src/models/mongo/models/dummyChildDbModel.ts
+++ src/models/mongo/models/dummyChildDbModel.ts
@@ -2,8 +2,9 @@ import { model, Document } from 'mongoose';
 import { DummyChildSchema } from '../schemas/dummyChildSchema';
 import { MongoCollections } from '../enums/mongoCollections';
 import { MongoModels } from '../enums/mongoModels';
+import { DummyChild } from '../../DummyChild';
 
-interface DummyChildDocument extends Document {}
+interface DummyChildDocument extends Document, DummyChild {}
 
 export const DummyChildDbModel = model<DummyChildDocument>(
   MongoModels.dummyChild,
--- src/models/mongo/models/dummyDbModel.ts
+++ src/models/mongo/models/dummyDbModel.ts
@@ -2,8 +2,9 @@ import { model, Document } from 'mongoose';
 import { DummySchema } from '../schemas/dummySchema';
 import { MongoCollections } from '../enums/mongoCollections';
 import { MongoModels } from '../enums/mongoModels';
+import { Dummy } from '../../Dummy';
 
-interface DummyDocument extends Document {}
+interface DummyDocument extends Document, Dummy {}
 
 export const DummyDbModel = model<DummyDocument>(
   MongoModels.dummy,
