649a422aa0adcb2dcd014020f4ffb7a5c30e0249
Gustavo <gloera@dfsupply.com>
2020-04-19 23:43:54 -0700

    add basic abstract repo

--- src/service/Repository.ts
+++ src/service/Repository.ts
@@ -1,26 +1,89 @@
-// use mongoose types
-import { Model, Document } from 'mongoose';
+import { Model, Document, MongooseFilterQuery } from 'mongoose';
 
-export class Repository<TModel> {
-  public constructor(private context: Model<Document>) {}
+export abstract class Repository<T extends Document> {
+  public constructor(private model: Model<T>) {}
 
-  public async add(model: TModel): Promise<TModel | void> {
-    const entity = new this.context(model);
-    await entity.save();
+  public async add(entity: T): Promise<T | null> {
+    const model = new this.model(entity);
+
+    try {
+      const result = await model.save();
+      return result;
+    } catch (err) {
+      return null;
+    }
   }
 
-  // add range
-
-  public async find(hash: string) {
-    const result = this.context.findOne({ hashKey: hash });
-    return result;
+  public async addRange(entities: T[]): Promise<T[] | null> {
+    try {
+      const result = await this.model.insertMany(entities);
+      if (result) {
+        return result;
+      }
+      return null;
+    } catch (err) {
+      return null;
+    }
   }
 
-  // findRange
+  public async find(
+    conditions: () => MongooseFilterQuery<T>
+  ): Promise<T | null> {
+    try {
+      const result = await this.model.findOne(conditions());
+      return result;
+    } catch (err) {
+      return null;
+    }
+  }
 
-  public async remove(hash: string) {}
+  public async findRange(
+    conditions: () => MongooseFilterQuery<T>
+  ): Promise<T[] | null> {
+    try {
+      const result = await this.model.find(conditions());
+      return result;
+    } catch (err) {
+      return null;
+    }
+  }
 
-  // removeRange
+  public async remove(id: string): Promise<boolean> {
+    try {
+      const result = await this.model.findByIdAndDelete(id);
 
-  public async update(hash: string, model: TModel) {}
+      if (!result) {
+        return false;
+      }
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  public async removeRange(
+    conditions: () => MongooseFilterQuery<T>
+  ): Promise<number> {
+    try {
+      const result = await this.model.deleteMany(conditions());
+      if (result) {
+        return result.deletedCount || 0;
+      }
+      return 0;
+    } catch (err) {
+      return 0;
+    }
+  }
+
+  public async update(id: string, newEntity: T): Promise<boolean> {
+    try {
+      const result = await this.model.findByIdAndUpdate(id, newEntity);
+      if (result) {
+        return true;
+      }
+      return false;
+    } catch (err) {
+      return false;
+    }
+  }
 }
